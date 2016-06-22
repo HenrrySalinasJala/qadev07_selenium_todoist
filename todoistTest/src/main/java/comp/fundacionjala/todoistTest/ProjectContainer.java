ea2a36de0574d7ab3db292bdc52ec6455ae25a14
HenrrySalinasJala <HenrrySalinas@fundacion-jala.org>
2016-06-22 19:14:10 -0400

    changes performed in class review

--- todoistTest/src/main/java/comp/fundacionjala/todoistTest/ProjectContainer.java
+++ todoistTest/src/main/java/comp/fundacionjala/todoistTest/ProjectContainer.java
@@ -42,4 +42,8 @@ public class ProjectContainer extends BasePage {
         lnkCancelTask.click();
         return new ProjectContainer();
     }
+
+    public boolean isProjectNameDisplayed() {
+        return lnkProjectName.isDisplayed();
+    }
 }
